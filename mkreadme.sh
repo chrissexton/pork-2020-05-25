d00ab89b41c8f9d39579232809d07da87c3da541
Chris Sexton <chrissexton@users.noreply.github.com>
2020-05-25 06:39:26 -0400

    maybe build html

--- mkreadme.sh
+++ mkreadme.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ls -bt | grep -v 'readme' | awk -F'\t' 'BEGIN {print "= img\n"} {print "* link:" $1 "[" $1 "]\n+\n[link=" $1 "]\nimage::" $1 "[width=500]" }'
+ls -bt | grep -v 'readme' | grep -v 'push' | grep -v 'index' | awk -F'\t' 'BEGIN {print "= img\n"} {print "* link:" $1 "[" $1 "]\n+\n[link=" $1 "]\nimage::" $1 "[width=500]" }'
